f6a47cc78ff6d755df5e626cb295ff769e894836
zhangkunyuan <zhangkunyuan@cmhi.chinamobile.com>
2023-10-20 10:35:55 +0800

    更新

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM python:3.9-alpine
 RUN apk add git
 WORKDIR /app
 COPY ./run.sh /app/init.sh
-CMD [ "init.sh" ]
+RUN chmod +x /app/init.sh
+CMD [ "/app/init.sh" ]
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/ash
 cd /app
 git clone --depth=1 https://github.com/zkytech/nas_tools.git
+cd nas_tools
 pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
 pip install -r ./requirements.txt
 python main.py
