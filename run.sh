4de48ce3aa92c51778a6bd6372dc12d60ced09c7
zhangkunyuan <zhangkunyuan@cmhi.chinamobile.com>
2024-05-07 11:22:39 +0800

    更新启动脚本

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/ash
 cd /app
+rm -rf nas_tools
 git clone --depth=1 https://github.com/zkytech/nas_tools.git
 cd nas_tools
 pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
